f4cc67313b96e4af5cec6bc066098b060b653575
asterich <asterich123@gmail.com>
2023-07-12 21:42:42 +0800

    wrote functions to handle WorkingYears

--- src/converter/doc_converter.go
+++ src/converter/doc_converter.go
@@ -2,9 +2,28 @@ package converter
 
 import (
 	"path/filepath"
+	"strings"
+	"time"
 
 	"github.com/asterich/CV-analyzer-backend/src/model"
 )
+
+func convertYearMonthStringToIntPair(yearMonth string) (int, int) {
+	if strings.Contains(yearMonth, "至今") {
+		return 2023, 4
+	}
+	if strings.Contains(yearMonth, "暑假") {
+		yearMonth = strings.Replace(yearMonth, "暑假", "7月", -1)
+	}
+	t, err := time.Parse("2006年1月2日", yearMonth)
+	if err != nil {
+		t, err = time.Parse("2006.1.2", yearMonth)
+		if err != nil {
+			return 0, 0
+		}
+	}
+	return t.Year(), int(t.Month())
+}
 
 func ExtractDegreeFromEducations(educations []model.Education) string {
 	// TODO: extract degree from educations
@@ -12,8 +31,16 @@ func ExtractDegreeFromEducations(educations []model.Education) string {
 }
 
 func ExtractWorkingYearsFromWorkExperiences(workExperiences []model.WorkExperience) int {
-	// TODO: extract working years from work experiences
-	return 0
+	totalWorkingMonths := 0
+	for _, workExperience := range workExperiences {
+		startYear, startMonth := convertYearMonthStringToIntPair(workExperience.Duration.Begin)
+		endYear, endMonth := convertYearMonthStringToIntPair(workExperience.Duration.End)
+		if startYear == 0 || endYear == 0 {
+			continue
+		}
+		totalWorkingMonths += (endYear-startYear)*12 + (endMonth - startMonth)
+	}
+	return (totalWorkingMonths + 12) / 12
 }
 
 func ConvertDocToCV(path string) (model.CV, error) {
@@ -34,22 +61,22 @@ func ConvertDocToCV(path string) (model.CV, error) {
 		Educations: []model.Education{
 			{
 				Duration: model.Duration{
-					Begin: 16,
-					End:   18,
+					Begin: "",
+					End:   "18",
 				},
 				School: "HUST",
 			},
 			{
 				Duration: model.Duration{
-					Begin: 16,
-					End:   18,
+					Begin: "16",
+					End:   "18",
 				},
 				School: "HUST",
 			},
 			{
 				Duration: model.Duration{
-					Begin: 16,
-					End:   18,
+					Begin: "16",
+					End:   "18",
 				},
 				School: "HUST",
 			},
@@ -57,8 +84,8 @@ func ConvertDocToCV(path string) (model.CV, error) {
 		WorkExperiences: []model.WorkExperience{
 			{
 				Duration: model.Duration{
-					Begin: 996,
-					End:   114514,
+					Begin: "996",
+					End:   "114514",
 				},
 				Experience: model.Experience{
 					CompanyOrOrganization: "Google, LLC.",
