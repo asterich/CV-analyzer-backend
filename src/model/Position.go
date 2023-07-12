2f976970729712988e03adb6dafa3cd8a797cfa4
asterich <asterich123@gmail.com>
2023-07-13 04:42:29 +0800

    fixed duration and removed filename field

--- src/model/Position.go
+++ src/model/Position.go
@@ -2,12 +2,17 @@ package model
 
 import "log"
 
+type IntDuration struct {
+	Begin int `gorm:"type:int" json:"begin"`
+	End   int `gorm:"type:int" json:"end"`
+}
+
 type Requirement struct {
-	Degree       string   `gorm:"type:varchar(20)" json:"degree"`
-	Age          Duration `gorm:"embedded;embeddedPrefix:age_" json:"age"`
-	WorkingYears Duration `gorm:"embedded;embeddedPrefix:working_years_" json:"working_years"`
-	Major        string   `gorm:"type:varchar(20)" json:"major"`
-	Others       string   `gorm:"type:varchar(512)" json:"others"`
+	Degree       string      `gorm:"type:varchar(20)" json:"degree"`
+	Age          IntDuration `gorm:"embedded;embeddedPrefix:age_" json:"age"`
+	WorkingYears IntDuration `gorm:"embedded;embeddedPrefix:working_years_" json:"working_years"`
+	Major        string      `gorm:"type:varchar(20)" json:"major"`
+	Others       string      `gorm:"type:varchar(512)" json:"others"`
 }
 
 // type Salary struct {
@@ -16,8 +21,8 @@ type Requirement struct {
 // }
 
 type Position struct {
-	ID           int    `gorm:"type:int;primaryKey" json:"id"`
-	Filename     string `gorm:"type:varchar(64)" json:"file_path"`
+	ID int `gorm:"type:int;primaryKey" json:"id"`
+	// Filename     string `gorm:"type:varchar(64)" json:"file_path"`
 	PositionName string `gorm:"type:varchar(20)" json:"position_name"`
 	// Company      string `gorm:"type:varchar(20)" json:"company"`
 	// Department string `gorm:"type:varchar(20)" json:"department"`
