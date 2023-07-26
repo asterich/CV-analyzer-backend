472c083aa2301aa954aaa1c5a95127e47fa21dd3
asterich <asterich123@gmail.com>
2023-07-26 21:31:12 +0800

    cors(although useless)

--- middleware/CORS.go
+++ middleware/CORS.go
@@ -9,14 +9,14 @@ import (
 func CORS() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		method := c.Request.Method
-		origin := c.Request.Header.Get("Origin")
-		if origin != "" {
-			c.Header("Access-Control-Allow-Origin", "*")
-			c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-			c.Header("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token, Authorization")
-			c.Header("Access-Control-Allow-Credentials", "true")
-			c.Set("content-type", "application/json")
-		}
+		// origin := c.Request.Header.Get("Origin")
+		// if origin != "" {
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		c.Header("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token, Authorization")
+		c.Header("Access-Control-Allow-Credentials", "true")
+		// 	c.Set("Content-Type", "application/json")
+		// }
 		//放行所有OPTIONS方法
 		if method == "OPTIONS" {
 			c.AbortWithStatus(http.StatusNoContent)
